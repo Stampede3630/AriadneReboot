51c2712ee5ea95f1c5f3f94cc614723868d8ee32
coder2343 <liamspencerwalsh@icloud.com>
2016-02-09 16:52:30 -0600

    pot comit
    return pot zero after init

--- src/org/usfirst/frc/team3630/robot/Potdegrees.java
+++ src/org/usfirst/frc/team3630/robot/Potdegrees.java
@@ -6,19 +6,22 @@ import edu.wpi.first.wpilibj.AnalogPotentiometer;
 
 public class Potdegrees {
 	Potentiometer pot;
+	double initDegrees ;
 	public Potdegrees(){
 		
 		pot = new AnalogPotentiometer(0, 360, 30);
 		AnalogInput ai = new AnalogInput(1);
 		pot = new AnalogPotentiometer(ai, 360, 30);
-		
+	
+		double initDegrees= pot.get();
 	
 		
 	}
 	public double fetchDegrees(){
 		
-		double degrees = pot.get();
+		double degrees = pot.get()-initDegrees;
 		return degrees;
+		
 		
 	}
 	
