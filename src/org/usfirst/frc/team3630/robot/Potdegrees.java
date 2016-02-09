d571d0c4aa9a707981a6836d302247f94692a62a
StampedeRobotics <breckstampede@gmail.com>
2016-02-09 15:22:31 -0600

    Potentiomoiter work
    New pot code

--- src/org/usfirst/frc/team3630/robot/Potdegrees.java
+++ src/org/usfirst/frc/team3630/robot/Potdegrees.java
@@ -1,8 +1,25 @@
 package org.usfirst.frc.team3630.robot;
 
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+
 public class Potdegrees {
+	Potentiometer pot;
 	public Potdegrees(){
+		
+		pot = new AnalogPotentiometer(0, 360, 30);
+		AnalogInput ai = new AnalogInput(1);
+		pot = new AnalogPotentiometer(ai, 360, 30);
+		
 	
-			
+		
 	}
-}
+	public double fetchDegrees(){
+		
+		double degrees = pot.get();
+		return degrees;
+	}
+	
+	}
+
