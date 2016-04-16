56695ddfb99029e3276e109dec52c5dc077b1831
coder2343 <liamspencerwalsh@icloud.com>
2016-04-16 16:27:40 -0500

    Update rpm

--- src/org/usfirst/frc/team3630/robot/Boulder_encoder.java
+++ src/org/usfirst/frc/team3630/robot/Boulder_encoder.java
@@ -4,24 +4,24 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
 public class Boulder_encoder{
-Encoder primenc;
+Encoder boulderEncoder;
 	
 	//requires the 
-	public Shooter_revolutions_per_minute(int input, int output) {
+	public Boulder_encoder(int input, int output) {
 	//Set up of shooter encoder based on standard settings from FRC
-		primenc = new Encoder(input, output, false, EncodingType.k1X);
-		primenc.setMaxPeriod(.1);
-		primenc.setMinRate(10);
-		primenc.setDistancePerPulse(235);
-		primenc.setReverseDirection(true); //this can be changed depending on encoder orientation
-		primenc.setSamplesToAverage(7);
+		 boulderEncoder = new Encoder(input, output, false, EncodingType.k1X);
+		 boulderEncoder.setMaxPeriod(.1);
+		 boulderEncoder.setMinRate(10);
+		 boulderEncoder.setDistancePerPulse(20);
+		 boulderEncoder.setReverseDirection(true); //this can be changed depending on encoder orientation
+		 boulderEncoder.setSamplesToAverage(7);
 	}
 	
 	//this method returns to 
-	public double degreesRot(){
-		double rpm_motor = primenc.getDistance();
-		double degrees = rpm_motor*1.5319;
-		return degrees;
+	public double RPM(){
+		double rps =  boulderEncoder.getDistance();
+		double rpm = rps*60;
+		return rpm;
 	}
 	
 }
