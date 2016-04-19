2a9a3fd71fe71abbf1b100965b34d24f22d1b29b
foxnard <foxnard@usfamily.net>
2016-04-18 20:49:18 -0500

    Shooter Motor RPMs part 2

--- src/org/usfirst/frc/team3630/robot/Boulder_encoder.java
+++ src/org/usfirst/frc/team3630/robot/Boulder_encoder.java
@@ -4,24 +4,37 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
 public class Boulder_encoder{
-Encoder primenc;
+	Encoder primenc;
 	
-	//requires the 
-	public Shooter_revolutions_per_minute(int input, int output) {
-	//Set up of shooter encoder based on standard settings from FRC
+	// Encoder for CIM Motor
+	// 2 channel quadrature output
+	// 20 pulses per channel per revolution 
+
+	// Constructor 
+	public Boulder_encoder(int input, int output) {
+		//Set up shooter encoder based on standard settings from FRC.
 		primenc = new Encoder(input, output, false, EncodingType.k1X);
-		primenc.setMaxPeriod(.1);
-		primenc.setMinRate(10);
-		primenc.setDistancePerPulse(235);
-		primenc.setReverseDirection(true); //this can be changed depending on encoder orientation
+		primenc.setMaxPeriod(.1);  // The maximum period (in seconds) where the device is still considered moving.
+		primenc.setMinRate(0.2); // This is in terms of distance per second (0.2 / sec = 2 ticks or 1/10th rotation).
+		primenc.setDistancePerPulse(0.05); // At 20 pulses (ticks) per revolution this gives a distance of 1 for 1 revolution.
+		primenc.setReverseDirection(false); //this can be changed depending on encoder orientation
 		primenc.setSamplesToAverage(7);
 	}
 	
-	//this method returns to 
-	public double degreesRot(){
-		double rpm_motor = primenc.getDistance();
-		double degrees = rpm_motor*1.5319;
-		return degrees;
+	public void reset() {
+		primenc.reset();
 	}
 	
+	// Want distance divided by time
+	public double rpm(){
+		// Get the current rate of the encoder. 
+		// Units are distance per second as scaled by the value from setDistancePerPulse().
+		// A distance of 1 is one rotation.
+		double rotationsPerSec = primenc.getRate();
+		double rotationsPerMin = rotationsPerSec * 60;
+		return rotationsPerMin;
+	}
+	
+	// The encoder will begin counting as soon as it is created. 
+	// To reset the encoder value to 0 call reset().
 }
